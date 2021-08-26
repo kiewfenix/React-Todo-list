96318f4325f667f98674e2e58c0baa27d8c6a7cf
kiewfenix <vasyl4964@gmail.com>
2021-08-26 15:33:58 +0300

    delete comments

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
-import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/';
 
-/* class Who extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            years: 26
-        }
-        this.nextYear = this.nextYear.bind(this);
-    }
-    nextYear(){
-        this.setState( state => ({years:++state.years})
-        )
-    }
-    render(){
-        return (
-            <>
-                <button onClick = {this.nextYear}>++</button>
-                <h1> My name is {this.props.name}, i`m {this.state.years} old</h1>
-            </>
-        )
-    }
-} 
-ReactDOM.render(<Who name= "Vasyl"/>, document.getElementById('root'));
-*/
 ReactDOM.render(<App/>, document.getElementById('root'));
